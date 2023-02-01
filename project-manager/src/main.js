fb92a2e63fa72412b2d5aaf55d7c195a5bbeb59d
mikotokoruk <misakaovo@163.com>
2023-02-02 02:40:45 +0800

    配置

--- project-manager/src/main.js
+++ project-manager/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -11,4 +15,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(ElementPlus)
+
 app.mount('#app')
+
