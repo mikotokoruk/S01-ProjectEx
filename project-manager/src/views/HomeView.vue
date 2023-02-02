db101a6e1d546b11ec7451c38e06b94cc66fe43e
mikotokoruk <misakaovo@163.com>
2023-02-02 22:11:41 +0800

    删去了不需要的布局

--- project-manager/src/views/HomeView.vue
+++ project-manager/src/views/HomeView.vue
@@ -3,7 +3,22 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<!--  <main>-->
+<!--    <TheWelcome />-->
+<!--  </main>-->
+  <div>
+    <div style="height: 60px; line-height: 60px; border-bottom: 1px solid #ccc; background-color: wheat"></div>
+
+    <div style="display: flex">
+      <div style="width :200px; min-height: calc(100vh - 60px); border-right: 1px solid #ccc">
+
+      </div>
+
+      <div style = "flex: 1, padding = 10px">
+        //主题
+      </div>
+
+    </div>
+
+  </div>
 </template>
